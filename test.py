1177e05804c0c39806968ed60f11a8ae8a2453a3
Tony Teate <tonyteate@gmail.com>
2014-03-07 02:53:28 -0500

    init

---
+++ test.py
@@ -0,0 +1,21 @@
+import os
+import pydocx
+
+root_dir = os.path.dirname(__file__)
+
+def filepath(filename):
+    return os.path.join(root_dir, filename)    
+
+def docx2html(path):
+    return pydocx.Docx2Html(path).parsed
+
+
+def docx2markdown(path):
+    return pydocx.Docx2Markdown(path).parsed
+
+def main():
+    print filepath("test.docx")
+    print docx2markdown("test.docx")
+
+if __name__ == '__main__':
+    main()
