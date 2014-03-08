63757def2df26135ab2495a2cec7dbdd26a39357
Tony Teate <tonyteate@gmail.com>
2014-03-08 03:43:19 -0500

    save

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ def docx2markdown(path):
     return pydocx.Docx2Markdown(path, True).parsed
 
 def main():
-    print docx2markdown("table.docx")
+    print docx2html("sample.docx")
 
 if __name__ == '__main__':
     main()
